3867daa273dadc383d6b8dcb73ff51d142d8f5ed
Yannis Haismann <yannishaismann@outlook.fr>
2021-06-20 19:54:32 +0200

    MOBILE TOP DONE v.1

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
+$('.top-site__nav').hide();
+
 $('.mobile-hamburger').on('click', function(){
     $('.top-site__nav').toggle();
+    $('.top-content').toggle();
 
     if($('.mobile-hamburger').css("opacity") == 0.5){
         $('.mobile-hamburger').css("opacity", 1);
     }else{
         $('.mobile-hamburger').css("opacity", 0.5);
     }
+
 });
