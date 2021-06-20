0c5062550685819aa3c72c85bad2bd119bcc7c0e
Yannis Haismann <yannishaismann@outlook.fr>
2021-06-20 20:25:47 +0200

    js update for multiple platform

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
-$('.top-site__nav').hide();
+var resizeTimeout;
+window.addEventListener('resize', function(event) {
+    if(window.innerWidth < 420 && window.innerWidth > 370){
+        clearTimeout(resizeTimeout);
+        resizeTimeout = setTimeout(function(){
+            window.location.reload();
+        }, 1000);
+    }
+});
 
 $('.mobile-hamburger').on('click', function(){
-    $('.top-site__nav').toggle();
     $('.top-content').toggle();
 
     if($('.mobile-hamburger').css("opacity") == 0.5){
         $('.mobile-hamburger').css("opacity", 1);
+        $('.top-site__nav').css('display', "none");
     }else{
+        $('.top-site__nav').css('display', "flex");
         $('.mobile-hamburger').css("opacity", 0.5);
     }
 
