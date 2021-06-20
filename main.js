5158105752d1b14eb1a4b7404e031e9354e65b67
Yannis Haismann <yannishaismann@outlook.fr>
2021-06-20 19:27:41 +0200

    NAV MOBILE

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 $('.mobile-hamburger').on('click', function(){
-    $('.top-nav').toggle();
+    $('.top-site__nav').toggle();
 
     if($('.mobile-hamburger').css("opacity") == 0.5){
         $('.mobile-hamburger').css("opacity", 1);
